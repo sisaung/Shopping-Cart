f33d603f78b37298ed4b7be6e5313b2dc0a5cbb6
sisaung <sisaungvipse780@gmail.com>
2024-08-19 13:59:09 +0630

    set up productSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./slice/categorySlice";
+import productSlice from "./slice/productSlice";
 
 export const store = configureStore({
   reducer: {
     category: categorySlice,
+    product: productSlice,
   },
 });
 
